90500943b4cb23cd4c9c6073e30f8451e27d5d9e
Jey <89442351+MayraPenafiel@users.noreply.github.com>
2022-07-23 13:23:14 -0500

    Subir Proyecto Farmacia 5 (Prueba)

---
+++ famacia_5/src/main/java/com/_prueba/famacia_5/Farmacia_5_Application.java
@@ -0,0 +1,12 @@
+package com._prueba.famacia_5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Farmacia_5_Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Farmacia_5_Application.class, args);
+    }
+}
---
+++ famacia_5/src/test/java/com/_prueba/famacia_6/Famacia6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com._prueba.famacia_6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Famacia6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
